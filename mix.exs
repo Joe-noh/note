05781a58d8717b2fda5e39f028cb7e253b928683
Joe-noh <honzawa.j@pepabo.com>
2015-09-29 19:22:08 +0900

    add blacksmith to deps

--- mix.exs
+++ mix.exs
@@ -12,31 +12,31 @@ defmodule Note.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
+    [mod: {Note, []}, applications: applications(Mix.env)]
+  end
+
+  def applications(:test) do
+    [:blacksmith | applications]
+  end
+
+  def applications(_), do: applications
+
+  def applications do
     [
-      mod: {Note, []},
-      applications: [
-        :phoenix,
-        :phoenix_html,
-        :cowboy,
-        :logger,
-        :phoenix_ecto,
-        :postgrex,
-        :comeonin
-      ]
+      :phoenix,
+      :phoenix_html,
+      :cowboy,
+      :logger,
+      :phoenix_ecto,
+      :postgrex,
+      :comeonin
     ]
   end
 
-  # Specifies which paths to compile per environment
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
   defp elixirc_paths(_),     do: ["lib", "web"]
 
-  # Specifies your project dependencies
-  #
-  # Type `mix help deps` for examples and options
   defp deps do
     [
       {:phoenix, "~> 1.0.2"},
@@ -47,7 +47,9 @@ defmodule Note.Mixfile do
       {:cowboy, "~> 1.0"},
 
       {:comeonin, "~> 1.2"},
-      {:guardian, "~> 0.6"}
+      {:guardian, "~> 0.6"},
+
+      {:blacksmith, "~> 0.1", only: :test}
     ]
   end
 end
