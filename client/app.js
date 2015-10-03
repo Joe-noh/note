7c48fef165b9daf7e9b5e99561ac9174e0724cde
Joe-noh <goflb.jh@gmail.com>
2015-10-03 21:19:56 +0900

    implement login

--- client/app.js
+++ client/app.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import {Router, Route, IndexRoute} from 'react-router'
+import {createHistory} from 'history'
 
 import App from './components/App'
+import Login from './components/Login'
 import PageIndex from './components/PageIndex'
+
+import auth from './libs/auth'
+
+let history = createHistory();
+
+let requireLogin = (nextState, replaceState) => {
+  if (!auth.isLoggedIn()) {
+    replaceState({nextPathname: nextState.location.pathname}, '/login');
+  }
+}
 
 React.render(
   (
-    <Router>
+    <Router history={history}>
       <Route path="/" component={App}>
-        <IndexRoute component={PageIndex} />
+        <Route path="login" component={Login} />
+        <IndexRoute component={PageIndex} onEnter={requireLogin} />
       </Route>
     </Router>
   ),
--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import auth from '../libs/auth'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: auth.isLoggedIn()
+    }
+  }
+
   render() {
     return (
       <div>
